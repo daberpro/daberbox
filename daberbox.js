ec6e68680909b20a99efbb7e83c27b5ef9595346
daberpro <daber.programing@gmail.com>
2022-03-05 17:29:33 +0700

    upload to github

---
+++ daberbox.js
@@ -0,0 +1,12 @@
+import { Main } from "./core.js";
+import { Physics } from "./utils/physics.js";
+import { World } from "./world.js";
+
+const daberbox = new Main();
+
+export {
+    daberbox,
+    World,
+    Physics
+};
+
